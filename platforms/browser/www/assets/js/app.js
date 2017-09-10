3970e530ed530fa0d41dca1ba936356d791769de
franzgusenbauer <franz.gusenbauer@gmail.com>
2017-09-10 19:34:41 +0000

    sixth commit

--- platforms/browser/www/assets/js/app.js
+++ platforms/browser/www/assets/js/app.js
@@ -4,6 +4,26 @@
 /* global vectoropts */
 /* global baseUrl */
 
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if (string.indexOf(substring) > -1){
+        alert('Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join('\n');
+
+        console.log(message);
+        alert(message);
+    }
+
+    return false;
+}
 
 
 var map, featureList, wartungenSearch = [];
